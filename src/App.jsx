a54b618e02a6165e3f6460dabc2dad0905215f3f
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-11-25 18:10:38 +0545

    add splash animation and integrate loader into app startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
+import Loader from "./components/Splash";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      )}
+    </>
   );
 };
 
